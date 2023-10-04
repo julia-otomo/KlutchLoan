5f3451ac8942c139439d246ba6710cb3cdd36326
Julia Otomo Duarte <julia_otomoduarte@outlook.com>
2023-10-04 13:53:20 -0300

    add desired_value validation in Solicitation view

--- solicitations/views.py
+++ solicitations/views.py
@@ -1,3 +1,4 @@
+from rest_framework.exceptions import ValidationError
 from rest_framework import generics
 from .models import Solicitation
 from .serializers import SolicitationSerializer
@@ -6,6 +7,18 @@ from .serializers import SolicitationSerializer
 class SolicitationListCreateView(generics.ListCreateAPIView):
     queryset = Solicitation.objects.all()
     serializer_class = SolicitationSerializer
+
+    def perform_create(self, serializer):
+        desired_value = serializer.validated_data.get("desired_value")
+
+        if desired_value:
+            if desired_value < 300 or desired_value > 10000:
+                raise ValidationError(
+                    {"message": "o valor desejado deve estar entre 300 e 10.000"}
+                )
+
+        serializer.save()
+        return super().perform_create(serializer)
 
 
 class SolicitationDetailsView(generics.RetrieveUpdateDestroyAPIView):
